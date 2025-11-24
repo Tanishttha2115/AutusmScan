95af9b880c11d845096d8db69db200544bf1f9e2
Tanishttha Sehgal <164616810+Tanishttha2115@users.noreply.github.com>
2025-11-24 22:17:41 +0530

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,15 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
+// Your Firebase configuration (autiscan-e3cff project)
 const firebaseConfig = {
-  apiKey: "AIzaSyBRIIRnUVPMhMRQOk-_Vlshee0N1235frI",
-  authDomain: "autiscan-f5f4c.firebaseapp.com",
-  projectId: "autiscan-f5f4c",
-  storageBucket: "autiscan-f5f4c.firebasestorage.app",
-  messagingSenderId: "354165945561",
-  appId: "1:354165945561:web:220b5ade27b4dd9e9a3ac8",
-  measurementId: "G-8WKN5Y4ZMB"
+  apiKey: "AIzaSyAgw1eC4hmV4ZdmrsBA9xR7CP5KgkyiwfU",
+  authDomain: "autiscan-e3cff.firebaseapp.com",
+  projectId: "autiscan-e3cff",
+  storageBucket: "autiscan-e3cff.firebasestorage.app",
+  messagingSenderId: "79337828773",
+  appId: "1:79337828773:web:1ba5efbf7ca5b50234384c",
+  measurementId: "G-KHPYTZ4165"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Firebase Services
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
